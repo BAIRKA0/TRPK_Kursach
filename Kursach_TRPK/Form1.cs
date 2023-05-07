d2c674b710ba5b0707cc8bae75174ad028045c9a
BAIRKA0 <91172074+BAIRKA0@users.noreply.github.com>
2023-05-07 22:49:30 +0800

    Add files via upload

---
+++ Kursach_TRPK/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kursach_TRPK
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadData()
+        {
+            //Data Source = IP_АДРЕС\\ИМЯ_СЕРВЕРА; Network Library = DBMSSOCN; Initial Catalog = ИМЯ_БД; User ID = ЛОГИН; Password = ПАРОЛЬ;
+            
+        }
+    }
+}
